08f219ab3d23f1c6052003cba1d3a97e0bb5673e
Eric Anderson <anderson.ericandrew@gmail.com>
2022-10-06 18:33:44 -0700

    add cart file and href to cart along with JSON file

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const shoppingCartValue = document.querySelector("shop-cart")
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -11,3 +12,8 @@ document.querySelectorAll(".nav-link").forEach(n => n.
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   }))
+
+// store each menu item in JSON and pull cost from there
+// use javascript to display cost of menu item
+// js to click on button and retreive price
+// cost will be simple loop through a list of all costs added to list
