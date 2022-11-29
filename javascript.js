2243ddf0f67c56d452f32215fa0093a03728a4a1
Eric Anderson <anderson.ericandrew@gmail.com>
2022-11-28 17:18:15 -0800

    add js for menu item component

--- javascript.js
+++ javascript.js
@@ -13,10 +13,33 @@ document.querySelectorAll(".nav-link").forEach(n => n.
     navMenu.classList.remove("active");
   }))
 
-//from stack overflow.. function to show visibility. learning JS...
-function klikaj(i) {
+function showModal(i) {
   document.getElementById(i).style.visibility='visible';
 }
+
+async function grabData() {
+  const res = await fetch('/menu.json');
+  const menu = await res.json();
+}
+
+grabData();
+
+const menuItem = document.createElement('template');
+menuItem.innerHTML = `
+  <div class="flex foods-border">
+    <div class="flex column food-container">
+      <h4 class="food-name">VVV Burger</h4>
+      <p class="flex food"></br>hawaiian bbq sauce, hash brown, white onion, sliced cheese, grilled pineapple</p>
+      <p class="price">$12.99</p>
+    </div>
+    <img src="images/b1.jpg" alt="vvv burger" style="padding: 20px;width: 100px; height: 100px">
+  </div>  
+`;
+
+document.getElementById('menuItems').appendChild(menuItem.content);
+
+
+
 
 // store each menu item in JSON and pull cost from there
 // use javascript to display cost of menu item
