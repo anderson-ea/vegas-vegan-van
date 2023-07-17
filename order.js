f83e6e609d0331a97d37b1684a534f92f745ff5d
Eric <anderson.ericandrew@gmail.com>
2023-07-17 11:14:59 -0700

    fix bug where calling from local storage cart list while it was empty

--- order.js
+++ order.js
@@ -8,13 +8,14 @@ const clearOrder = () => {
   location.reload()
 }
 
-let printList = cartList.replace(/[\[\]"]/g, '')
-let realPrintList = printList.replace(/,/g, ", ")
-
-pageContent.innerHTML = `
-  <div class="flex column" style="align-items: center; justify-content: space-between; height: 20vh; line-height: 3rem; text-align: center; justify-content: center; padding-top: 20px;">
-    Your order total comes out to $${total}
-    </br>Items Ordered: ${realPrintList}
-    <button class="orderButton" style="width: 200px; margin: 20px;" onclick="clearOrder()">Cancel Order</button>
-  </div>
-`
+if (cartList.length > 0) {
+  let printList = cartList.replace(/[\[\]"]/g, '')
+  let realPrintList = printList.replace(/,/g, ", ")
+  pageContent.innerHTML = `
+    <div class="flex column" style="align-items: center; justify-content: space-between; height: 20vh; line-height: 3rem; text-align: center; justify-content: center; padding-top: 20px;">
+      Your order total comes out to $${total}
+      </br>Items Ordered: ${realPrintList}
+      <button class="orderButton" style="width: 200px; margin: 20px;" onclick="clearOrder()">Cancel Order</button>
+    </div>
+  `
+}
